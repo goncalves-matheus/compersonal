1d6253d858ec9317828869513824e224c3fd8fca
Gabriel <gabriel.sobreir_BOLS@compasso.com.br>
2021-04-07 14:36:58 -0300

    Projeto criado

---
+++ src/test/java/compasso/estagio/grupo/projeto5/Telas/TelasApplicationTests.java
@@ -0,0 +1,13 @@
+package compasso.estagio.grupo.projeto5.Telas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
