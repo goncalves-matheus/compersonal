77d9f6ce14cf7160db860eb545543874b46e4d7a
Gabriel <gabriel.sobreir_BOLS@compasso.com.br>
2021-04-07 22:40:54 -0300

    Cadastro, Login, Recuperacao e Home feitas

---
+++ src/main/java/compasso/estagio/grupo/projeto5/Telas/controller/loginController.java
@@ -0,0 +1,20 @@
+package compasso.estagio.grupo.projeto5.Telas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class loginController {
+
+	@RequestMapping(method = RequestMethod.GET, value = "/login")
+	public String login() {
+		return "login";
+	}
+	
+	@RequestMapping("/recuperar")
+	public String recupera() {
+		return "recuperacao";
+	}
+	
+}
