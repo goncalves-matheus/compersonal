0867ee4697ca60356b1ff378fca59ef29a474cd4
Matheus <matheus.goncalv_BOLS@compasso.com.br>
2021-04-12 13:57:10 -0300

    Controller planos

--- src/main/java/compasso/estagio/grupo/projeto5/Telas/controller/PlanosController.java
+++ src/main/java/compasso/estagio/grupo/projeto5/Telas/controller/PlanosController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping("aulas")
-public class AulasController {
+@RequestMapping("planos")
+public class PlanosController {
     
     @GetMapping
-    public String aulas(){
-        return "aulas";
+    public String planos(){
+        return "pagar";
     }
 }
