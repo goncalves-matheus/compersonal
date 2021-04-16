502456ff629b54e9d83a5d06ccb6dd82a424adb2
Alexandre Lopes <alexandre.lopes_bols@compasso.com.br>
2021-04-16 16:59:59 -0300

    Edição de Nome e Sobrenome na página Perfil e padronização da cor dos botões de inseriraula

--- src/main/java/compasso/estagio/grupo/projeto5/Telas/controller/PerfilController.java
+++ src/main/java/compasso/estagio/grupo/projeto5/Telas/controller/PerfilController.java
@@ -1,13 +1,20 @@
 package compasso.estagio.grupo.projeto5.Telas.controller;
 
+import java.util.Objects;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import compasso.estagio.grupo.projeto5.Telas.dto.UsuarioDto;
 import compasso.estagio.grupo.projeto5.Telas.model.Perfil;
 import compasso.estagio.grupo.projeto5.Telas.repository.PerfilRepository;
 
@@ -20,20 +27,38 @@ public class PerfilController {
 
 	@GetMapping
 	public String aulas(Model modelo) {
-
+		
 		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		String email;
 
 		if (principal instanceof UserDetails) {
 			email = ((UserDetails) principal).getUsername();
 			Perfil perfil = repository.findByEmail(((UserDetails) principal).getUsername());
-			modelo.addAttribute("nome", perfil.getPrimeiroNome());
-			modelo.addAttribute("sobrenome", perfil.getUltimoNome());
+			modelo.addAttribute("primeiroNome", perfil.getPrimeiroNome());
+			modelo.addAttribute("ultimoNome", perfil.getUltimoNome());
 			modelo.addAttribute("email", email);
 		} else {
 			email = principal.toString();
 		}
 
 		return "perfil";
+	}
+	
+	@PostMapping("/")
+	public String alterar(@Valid UsuarioDto usuarioDto, BindingResult result) {
+//		System.out.println("O nome é " + usuarioDto.getPrimeiroNome() + " !!!");
+		
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		if (principal instanceof UserDetails) {
+			Perfil perfil = repository.findByEmail(((UserDetails) principal).getUsername());
+			if (Objects.nonNull(perfil)) {
+				perfil.setPrimeiroNome(usuarioDto.getPrimeiroNome());
+				perfil.setUltimoNome(usuarioDto.getUltimoNome());
+				repository.save(perfil);
+				
+			}
+		}
+				
+		return "redirect:/perfil";
 	}
 }
