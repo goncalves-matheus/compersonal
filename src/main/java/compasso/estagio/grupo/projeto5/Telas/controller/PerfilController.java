eb0d472dc5bcdd2b84ab085150e704fb9ad0f49a
Gabriel Jefferson Silva Calixto Sobreira <gabriel.sobreir_bols@compasso.com.br>
2021-04-16 23:17:00 +0000

    PerfilController.java conflito resolvido

--- src/main/java/compasso/estagio/grupo/projeto5/Telas/controller/PerfilController.java
+++ src/main/java/compasso/estagio/grupo/projeto5/Telas/controller/PerfilController.java
@@ -1,12 +1,13 @@
 package compasso.estagio.grupo.projeto5.Telas.controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import compasso.estagio.grupo.projeto5.Telas.model.Perfil;
 import compasso.estagio.grupo.projeto5.Telas.repository.PerfilRepository;
 
@@ -21,11 +22,8 @@ public class PerfilController {
 	public String aulas(Model modelo, Principal principal) {
 
 		Perfil perfil = repository.findByEmail(principal.getName());
-
 		modelo.addAttribute("nome", perfil.getPrimeiroNome());
-
 		modelo.addAttribute("sobrenome", perfil.getUltimoNome());
-
 		modelo.addAttribute("email", principal.getName());
 
 		return "perfil";
