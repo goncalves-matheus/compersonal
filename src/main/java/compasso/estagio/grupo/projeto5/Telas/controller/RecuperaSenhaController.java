fb09211c9818b2c891702c4b175ae8d82c5e6dd1
Matheus <matheus.goncalv_BOLS@compasso.com.br>
2021-04-16 18:16:50 -0300

    Classe separada para fazer a persistencia dos dados dto

--- src/main/java/compasso/estagio/grupo/projeto5/Telas/controller/RecuperaSenhaController.java
+++ src/main/java/compasso/estagio/grupo/projeto5/Telas/controller/RecuperaSenhaController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import compasso.estagio.grupo.projeto5.Telas.dto.AuxiliarRecuperaSenhaDto;
 import compasso.estagio.grupo.projeto5.Telas.dto.RecuperaSenhaDto;
 import compasso.estagio.grupo.projeto5.Telas.model.Usuario;
 import compasso.estagio.grupo.projeto5.Telas.repository.UsuarioRepository;
@@ -26,7 +27,7 @@ public class RecuperaSenhaController {
     @Autowired
     private EnviaEmailService emailSerive;
     
-    private RecuperaSenhaDto recuperaSenhaDtoAuxiliar = new RecuperaSenhaDto();
+    private AuxiliarRecuperaSenhaDto auxiliarRecuperaSenhaDto;
 
     @GetMapping
     public String novoCadastro(RecuperaSenhaDto recuperaSenhaDto) {
@@ -34,11 +35,12 @@ public class RecuperaSenhaController {
     }
      
     @PostMapping("email")
-    public String enviarEmail(RecuperaSenhaDto recuperaSenhaDto){
-        Optional<Usuario> usuario = usuarioRepository.findByEmail(recuperaSenhaDto.getEmail());
+    public String email(RecuperaSenhaDto recuperaSenhaDto){
+        String email = recuperaSenhaDto.getEmail();
+        Optional<Usuario> usuario = usuarioRepository.findByEmail(email);
         if(usuario.isPresent()){
-            this.recuperaSenhaDtoAuxiliar.setEmail(recuperaSenhaDto.getEmail());
-            this.recuperaSenhaDtoAuxiliar.setCodigoEnviadoPorEmail(emailSerive.enviarCodigo(recuperaSenhaDto.getEmail()));
+            String codigo = emailSerive.enviarCodigo(email);
+            this.auxiliarRecuperaSenhaDto = new AuxiliarRecuperaSenhaDto(email, codigo);
             return "trocar_senha";
         }
         return "recuperacao";
@@ -46,15 +48,12 @@ public class RecuperaSenhaController {
 
     @PostMapping("codigo")
     public String mudarSenha(@Valid RecuperaSenhaDto recuperaSenhaDto, BindingResult resultado) {
-        recuperaSenhaDto.setEmail(this.recuperaSenhaDtoAuxiliar.getEmail());
-        recuperaSenhaDto.setCodigoEnviadoPorEmail(this.recuperaSenhaDtoAuxiliar.getCodigoEnviadoPorEmail());
+        
+        recuperaSenhaDto = this.auxiliarRecuperaSenhaDto.atualizarDto(recuperaSenhaDto);
         
         if (resultado.hasErrors() || !recuperaSenhaDto.validarCodigo()) {
-            System.out.println(recuperaSenhaDto.getEmail());
             return "recuperacao";
         }
-
-        System.out.println(recuperaSenhaDto.getEmail());
         Optional<Usuario> usuarioOptional = usuarioRepository.findByEmail(recuperaSenhaDto.getEmail());
         if (usuarioOptional.isPresent()) {
             Usuario usuario = usuarioOptional.get();
