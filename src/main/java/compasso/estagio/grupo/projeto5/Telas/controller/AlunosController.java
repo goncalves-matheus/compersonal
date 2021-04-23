4be89e71d5852615acb4860ec01636fa0f12e468
marcel_sales <marcel.sales_BOLS@compasso.com.br>
2021-04-23 12:35:29 -0300

    placeholder_no_inseriraula.html

--- src/main/java/compasso/estagio/grupo/projeto5/Telas/controller/AlunosController.java
+++ src/main/java/compasso/estagio/grupo/projeto5/Telas/controller/AlunosController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import compasso.estagio.grupo.projeto5.Telas.dto.InformacaoAdicionalDto;
 import compasso.estagio.grupo.projeto5.Telas.dto.UsuarioDto;
 import compasso.estagio.grupo.projeto5.Telas.model.Perfil;
--- src/main/java/compasso/estagio/grupo/projeto5/Telas/controller/AulasController.java
+++ src/main/java/compasso/estagio/grupo/projeto5/Telas/controller/AulasController.java
@@ -4,14 +4,12 @@ import java.security.Principal;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import compasso.estagio.grupo.projeto5.Telas.dto.MensagemDto;
 import compasso.estagio.grupo.projeto5.Telas.model.Mensagem;
 import compasso.estagio.grupo.projeto5.Telas.model.Perfil;
