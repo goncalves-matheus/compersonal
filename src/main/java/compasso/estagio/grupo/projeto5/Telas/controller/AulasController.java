59d0da56218d0c42ad3eae486902fa016b81ebde
Matheus <matheus.goncalv_BOLS@compasso.com.br>
2021-04-24 15:37:19 -0300

    Finalização do chat

--- src/main/java/compasso/estagio/grupo/projeto5/Telas/controller/AulasController.java
+++ src/main/java/compasso/estagio/grupo/projeto5/Telas/controller/AulasController.java
@@ -1,10 +1,6 @@
 package compasso.estagio.grupo.projeto5.Telas.controller;
 
 import java.security.Principal;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import compasso.estagio.grupo.projeto5.Telas.dto.MensagemDto;
 import compasso.estagio.grupo.projeto5.Telas.model.GestorDeMensagens;
-import compasso.estagio.grupo.projeto5.Telas.model.Mensagem;
-import compasso.estagio.grupo.projeto5.Telas.model.Perfil;
 import compasso.estagio.grupo.projeto5.Telas.model.Tipo;
 import compasso.estagio.grupo.projeto5.Telas.repository.AulaRepository;
 import compasso.estagio.grupo.projeto5.Telas.repository.MensagemRepository;
@@ -24,7 +18,7 @@ import compasso.estagio.grupo.projeto5.Telas.repository.PerfilRepository;
 
 @Controller
 @RequestMapping("aulas")
-public class AulasController {
+public class AulasController extends GestorDeMensagens {
 
 	@Autowired
 	AulaRepository aulaRepository;
@@ -34,8 +28,6 @@ public class AulasController {
 
 	@Autowired
 	PerfilRepository perfilRepository;
-
-	GestorDeMensagens gestorDeMensagens = new GestorDeMensagens();
 
 	@GetMapping
 	public String aulas(Model modelo, MensagemDto mensagemDto, Principal principal) {
@@ -53,26 +45,11 @@ public class AulasController {
 		modelo.addAttribute("perna", aulaRepository.findByTipo(Tipo.PERNAS));
 		modelo.addAttribute("braco", aulaRepository.findByTipo(Tipo.BRACOS));
 		modelo.addAttribute("peito", aulaRepository.findByTipo(Tipo.PEITO));
-
+		
+		super.setRepositories(this.perfilRepository, this.mensagemRepository);
 		carregarMensagensDoChat(modelo, perfilRepository.findByEmail(principal.getName()));
 
 		return "aulas";
 	}
-	
-	public void carregarMensagensDoChat(Model modelo, Perfil aluno) {
-        Perfil personal = perfilRepository.findByPermissaoPermissao("Personal");
-
-        List<Mensagem> mensagensDoAluno = mensagemRepository.findByPerfilId(aluno.getId());
-        List<Mensagem> mensagensDoPersonal = mensagemRepository.findByPerfilIdDestinatarioId(personal.getId(), aluno.getId());
-        List<Mensagem> todasAsMensagens = Stream.concat(mensagensDoAluno.stream(), mensagensDoPersonal.stream()).collect(Collectors.toList());
-
-        Collections.sort(todasAsMensagens);
-
-        modelo.addAttribute("perfil", aluno);
-        modelo.addAttribute("personal", personal);
-
-        modelo.addAttribute("mensagens", todasAsMensagens);
-        modelo.addAttribute("horaFinal", todasAsMensagens.get(todasAsMensagens.size()-1).getHoraFormatada());
-    }
 
 }
--- src/main/java/compasso/estagio/grupo/projeto5/Telas/controller/MensagemController.java
+++ src/main/java/compasso/estagio/grupo/projeto5/Telas/controller/MensagemController.java
@@ -1,10 +1,6 @@
 package compasso.estagio.grupo.projeto5.Telas.controller;
 
 import java.security.Principal;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.validation.Valid;
 
@@ -18,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import compasso.estagio.grupo.projeto5.Telas.dto.MensagemDto;
-import compasso.estagio.grupo.projeto5.Telas.dto.UsuarioDto;
 import compasso.estagio.grupo.projeto5.Telas.model.GestorDeMensagens;
 import compasso.estagio.grupo.projeto5.Telas.model.Mensagem;
 import compasso.estagio.grupo.projeto5.Telas.model.Perfil;
@@ -27,15 +22,13 @@ import compasso.estagio.grupo.projeto5.Telas.repository.PerfilRepository;
 
 @Controller
 @RequestMapping("mensagem")
-public class MensagemController {
+public class MensagemController extends GestorDeMensagens {
 
     @Autowired
-    private PerfilRepository perfilRepository;
+    PerfilRepository perfilRepository;
 
     @Autowired
-    private MensagemRepository mensagemRepository;
-
-    GestorDeMensagens gestorDeMensagens = new GestorDeMensagens();
+    MensagemRepository mensagemRepository;
 
     @GetMapping()
     public String chatPersonal(MensagemDto mensagemDto, Model modelo, Principal principal) {
@@ -43,19 +36,20 @@ public class MensagemController {
     }
 
     @GetMapping("{idAluno}")
-    public String carregarMensagensDeCadaAlunoParaOPersonal(@PathVariable String idAluno, MensagemDto mensagemDto, Model modelo, Principal principal) {
+    public String carregarMensagensDoAluno(@PathVariable String idAluno, MensagemDto mensagemDto, Model modelo, Principal principal) {
+        super.setRepositories(this.perfilRepository, this.mensagemRepository);
         Perfil perfilDoALuno = perfilRepository.findByEmail(idAluno);
 
         modelo.addAttribute("alunos", listarAlunos(principal));
         modelo.addAttribute("hora", mensagemRepository.findByPerfilId(perfilRepository.findByEmail(idAluno).getId()).get(1).getHoraFormatada());
+        
         carregarMensagensDoChat(modelo, perfilDoALuno);
 
         return "chat-personal";
     }
 
     @PostMapping("novaMensagemPersonal")
-    public String novaMensagemPersonal(@Valid MensagemDto mensagemDto, String email, Model modelo, BindingResult result,
-            Principal principal) {
+    public String novaMensagemDoPersonal(@Valid MensagemDto mensagemDto, String email, Model modelo, BindingResult result, Principal principal) {
         if (result.hasErrors()) {
             return "chat-personal";
         }
@@ -68,13 +62,14 @@ public class MensagemController {
     }
 
     @PostMapping("novaMensagemAluno")
-    public String novaMensagemAluno(@Valid MensagemDto mensagemDto, Model modelo, BindingResult result,
-            Principal principal) {
+    public String novaMensagemDoAluno(@Valid MensagemDto mensagemDto, Model modelo, BindingResult result, Principal principal) {
+        super.setRepositories(this.perfilRepository, this.mensagemRepository);
         if (result.hasErrors()) {
             return "redirect:/aulas";
         }
         Mensagem mensagem = mensagemDto.toMensagem();
         mensagem.setDestinatarioId(perfilRepository.findByPermissaoPermissao("Personal").getId());
+        
         salvarMensagem(mensagem, principal);
 
         Perfil perfil = perfilRepository.findByEmail(principal.getName());
@@ -82,34 +77,5 @@ public class MensagemController {
 
         return "redirect:/aulas";
     }
-    public void salvarMensagem(Mensagem mensagem, Principal principal) {
-        Perfil perfil = perfilRepository.findByEmail(principal.getName());
-        mensagem.setPerfil(perfil);
-        perfil.setMensagens(mensagem);
-        mensagemRepository.save(mensagem);
-    }
 
-    public void carregarMensagensDoChat(Model modelo, Perfil aluno) {
-        Perfil personal = perfilRepository.findByPermissaoPermissao("Personal");
-
-        List<Mensagem> mensagensDoAluno = mensagemRepository.findByPerfilId(aluno.getId());
-        List<Mensagem> mensagensDoPersonal = mensagemRepository.findByPerfilIdDestinatarioId(personal.getId(), aluno.getId());
-        List<Mensagem> todasAsMensagens = Stream.concat(mensagensDoAluno.stream(), mensagensDoPersonal.stream()).collect(Collectors.toList());
-
-        Collections.sort(todasAsMensagens);
-
-        modelo.addAttribute("perfil", aluno);
-        modelo.addAttribute("personal", personal);
-
-        modelo.addAttribute("mensagens", todasAsMensagens);
-        modelo.addAttribute("horaFinal", todasAsMensagens.get(todasAsMensagens.size()-1).getHoraFormatada());
-    }
-
-    public List<UsuarioDto> listarAlunos(Principal principal) {
-        List<Perfil> perfis = perfilRepository.findAll();
-        List<Perfil> personais = perfilRepository.findByPermissao(perfilRepository.findByEmail(principal.getName()).getPermissao());
-        perfis.removeAll(personais);
-        List<UsuarioDto> alunos = UsuarioDto.converte(perfis);
-        return alunos;
-    }
 }
