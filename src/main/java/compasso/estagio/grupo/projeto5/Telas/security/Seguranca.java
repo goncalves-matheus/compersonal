937731ae9392a73f525f600d7dde9422ca9705ad
Matheus <matheus.goncalv_BOLS@compasso.com.br>
2021-04-25 20:34:47 -0300

    Correção nas configurações de segurança das mensagens

--- src/main/java/compasso/estagio/grupo/projeto5/Telas/security/Seguranca.java
+++ src/main/java/compasso/estagio/grupo/projeto5/Telas/security/Seguranca.java
@@ -1,7 +1,10 @@
 package compasso.estagio.grupo.projeto5.Telas.security;
 
+import java.lang.reflect.Method;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,7 +25,10 @@ public class Seguranca extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers("/*.css", "/*.jpeg","/*.js").permitAll()
 				.antMatchers("/", "/recuperar/**","/cadastro", "/cadastro/*", "/planos").permitAll()
 				.antMatchers("/dashboard/aluno").hasAuthority("Usuario")
-				.antMatchers("/dashboard/personal","/inseriraula/**","/alunos","/mensagem/**").hasAuthority("Personal")
+				.antMatchers("/dashboard/personal","/inseriraula/**","/alunos").hasAuthority("Personal")
+				.antMatchers(HttpMethod.GET,"/mensagem/**").hasAuthority("Personal")
+				.antMatchers(HttpMethod.POST,"/mensagem/novaMensagemPersonal").hasAuthority("Personal")
+				.antMatchers(HttpMethod.POST,"/mensagem/novaMensagemAluno").hasAuthority("Usuario")
 				.anyRequest().authenticated()
 				.and()
 				.formLogin(form -> form.loginPage("/login").permitAll().defaultSuccessUrl("/", true))
